21190d245d9454e83c2fdcbf5b8218f134565e41
Tanishka Jain <tanishkajain1508@gmail.com>
2023-12-10 23:42:39 +0530

    ADDED:UC_2_CREATE_ADDRESS_BOOK_TABLE

--- address_book.sql
+++ address_book.sql
@@ -3,3 +3,16 @@ CREATE DATABASE address_book_service;
 USE address_book_service;
 SHOW databases;
 SELECT DATABASE();
+
+-- <----------------USE CASE 2----------->
+CREATE TABLE AddressBook (
+    ContactID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Address VARCHAR(255),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    ZipCode VARCHAR(10),
+    PhoneNumber VARCHAR(15),
+    Email VARCHAR(100)
+);
